af4e133cef5f768f7fdb03c9f118a2e1ac391cc9
colerquinn <cole.r.quinn@outlook.com>
2026-03-17 14:40:39 -0700

    MSSA

--- MSSAProject/MSSAProject2.ps1
+++ MSSAProject/MSSAProject2.ps1
@@ -1,6 +1,7 @@
 #List Ip Address for a Remote Host
 $Computer = Read-Host "Enter the Remote Computer Name"
-try { $IPInformation = Get-CimInstance -ClassName Win32_NetworkAdapterConfiguration `
+try {
+    $IPInformation = Get-CimInstance -ClassName Win32_NetworkAdapterConfiguration `
     -Filter "IPEnabled = TRUE" -ComputerName $Computer -ErrorAction Stop
     Write-Host "IP Addresses for $Computer :" -ForegroundColor Green $IPInformation.IPAddress
 }
@@ -8,3 +9,41 @@ catch {
     Write-Host "Unable to retrive IP Information for $computer" -ForegroundColor Red
 }
 
+#Retrieve Network Adapter Properties for Remote Computers
+try {
+    $Adapters = Get-CimInstance -ClassName Win32_NetworkAdapterConfiguration`
+    -Filter "IPEnabled = TRUE" -ComputerName $Computer -ErrorAction Stop
+
+    Write-Host "Network Adapter Properties for $Computer :" -ForegroundColor Cyan
+    $Adapters | Select-Object `
+                Description,
+                MACAddress,
+                IPAddress,
+                IPSubnet,
+                DefaultIPGateway,
+                DNSServerSearchOrder,
+                DHCPEnabled
+    }
+    catch {
+        Write-Host "Unable to retrieve network adapter information for $Computer" -ForegroundColor Red
+}
+
+#Release and Renew DHCP Leases on Adapters
+try { 
+    $Adapters = Get-CimInstance -ClassName Win32_NetworkAdapterConfiguration`
+    -Filter "IPEnabled = TRUE AND DHCPEnabled = TRUE"`
+    -ComputerName $Computer -ErrorAction Stop
+
+    Write-Host "Releasing and Renewing DHCP Leases on $Computer . . ." -ForegroundColor Yellow
+
+        foreach ($Adapter in $Adapters) {
+            $Adapter.ReleaseDHCPLease()
+            Start-Sleep -Seconds 2
+            $Adapter.RenewDHCPLease()
+        }
+    }
+    catch {
+        Write-Host "Action Failed on $Computer" -ForegroundColor Red
+}
+
+#
