28246bf144c32f50d576b69213c760bc46f36fb5
colerquinn <cole.r.quinn@outlook.com>
2026-03-17 11:18:00 -0700

    MSSA

--- MSSAProject/MSSAProject1.ps1
+++ MSSAProject/MSSAProject1.ps1
@@ -12,11 +12,12 @@ foreach ($OU in $OUs) {
     $ExistingOU =  Get-ADOrganizationalUnit -Filter "Name -eq '$OU'"
         if ($ExistingOU -eq $null) {
             New-ADOrganizationalUnit -Name $OU -Path $DomainDN
-            Write-Host "New OU created with the name: $OU"
+            Write-Host "New OU created with the name: $OU" -ForegroundColor Green
         } else {
-            Write-Host "OU already exists: $OU"
+            Write-Host "OU already exists: $OU" -ForegroundColor Yellow
         }
 }
+
 #Create New Groups
 $Groups = @(
     @{ Name = "HR-Users"; OU = "HumanResources" }
@@ -30,11 +31,12 @@ foreach ($Group in $Groups) {
     $GroupPath = "OU=$($Group.OU),$DomainDN"
         if ($ExistingGroup -eq $null) {
             New-ADGroup -Name $($Group.Name) -GroupScope Global -GroupCategory Security -Path $GroupPath
-            Write-Host "New Global Security Group has been created with the name: $($Group.Name)"
+            Write-Host "New Global Security Group has been created with the name: $($Group.Name)" -ForegroundColor Green
         } else {
-            Write-Host "The Group already exists: $($Group.Name)"
+            Write-Host "The Group already exists: $($Group.Name)" -ForegroundColor Yellow
         }
 }
+
 #Create Bulk Users
 $Users = @(
     @{ Name = "Cole Quinn"; Sam = "cquinn"; OU = "IT"}
@@ -73,8 +75,9 @@ foreach ($User in $Users) {
     -Path $UserPath `
     -AccountPassword $Password `
     -Enabled $true
-    Write-Host "New User created: $($User.Name) ($($User.Sam)) in OU $($User.OU)"
+    Write-Host "New User created: $($User.Name) ($($User.Sam)) in OU $($User.OU)" -ForegroundColor Green
 }
+
 #Add Existing Users into Existing Groups
 $GroupMap = @{
     "IT" = "IT-Admins"
@@ -83,28 +86,30 @@ $GroupMap = @{
     "Sales" = "Sales-Team"
     "Training" = "Training-Security"
 }
-ForEach ($User in $Users) {
+foreach ($User in $Users) {
     $GroupName = $GroupMap[$User.OU]
     $ExistingGroup = Get-ADGroup -Filter "Name -eq '$GroupName'"
         if ($ExistingGroup) {
         Add-ADGroupMember -Identity $GroupName -Members $User.Sam
-        Write-Host "$($User.Sam) has been added to: $GroupName"
+        Write-Host "$($User.Sam) has been added to: $GroupName" -ForegroundColor Green
         } 
         else {
         Write-Host "Could not complete this action." -ForegroundColor Red
     }
 }
+
 #Get Disabled Users and Enable Them
 $DisabledUsers = Get-ADUser -Filter 'Enabled -eq $false'
-ForEach ($User in $DisabledUsers) {
+foreach ($User in $DisabledUsers) {
     try {
         Enable-ADAccount -Identity $User.SamAccountName
-        Write-Host "User Enabled: $($User.SamAccountName)"
+        Write-Host "User Enabled: $($User.SamAccountName)" -ForegroundColor Green
     }
     catch {
-        Write-Host "Failed to Enable User: $($User.SamAccountName)"
+        Write-Host "Failed to Enable User: $($User.SamAccountName)" -ForegroundColor Red
     }
 }
+
 #Disable user accounts that have that have not been used to logon with in 30 or more days
 $Cutoff = (Get-Date).AddDays(-30)
 $CutoffFileTime = $Cutoff.ToFileTime()
@@ -112,27 +117,45 @@ $InactiveUsers = Get-ADUser -Filter "LastLogonTimeStamp -lt $CutoffFileTime" -Pr
 foreach ($User in $InactiveUsers) {
         try {
             Disable-ADAccount -Identity $User.SamAccountName
-            Write-Host "Disabled user: $($User.SamAccountName)"
+            Write-Host "Disabled user: $($User.SamAccountName)" -ForegroundColor Green
         }
         catch {
         Write-Host "Failed to disable user: $($User.SamAccountName)" -ForegroundColor Red
          }
 }
+
 #Disabled User
 Disable-ADAccount -Identity "cquinn"
+
 #Move Disabled Users to the DisabledUsers OU
 $TargetDisabledOU = "OU=DisabledUsers,DC=adatum,DC=com"
 $DisabledUsers2 = Get-ADUser -Filter 'Enabled -eq $false' -Properties DistinguishedName
 foreach ($User in $DisabledUsers2) {
     try {
         Move-ADObject -Identity $User.DistinguishedName -TargetPath $TargetDisabledOU
-        Write-Host "Moved user: $($User.SamAccountName) to DisabledUsers OU"
+        Write-Host "Moved user: $($User.SamAccountName) to DisabledUsers OU" -ForegroundColor Green
     }
     catch {
         Write-Host "Failed to move user: $($User.SamAccountName)" -ForegroundColor Red
     }
 }
+
 #Create list of computers with a particular operating system installed
 $OS = "Windows Server 2022*"
 $Windows2022Computers = Get-ADComputer -Filter "OperatingSystem -like '$OS'" -Properties OperatingSystem
 $Windows2022Computers | Select-Object Name,OperatingSystem
+
+#Restart a Computer Remotely
+$Computers = @(
+    "Server01"
+    "Server02"
+    "Server03"
+)
+foreach ($computer in $computers) {
+    try { Restart-Computer -ComputerName $Computer -Force
+    Write-Host "Successfully Restarted Computer: $Computer" -ForegroundColor Green
+    }
+    catch { 
+        Write-Host "Unable to Restart Computer: $Computer" -ForegroundColor Red
+    }
+}
