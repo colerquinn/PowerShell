52a2140f494f8c537cfc51f04758c9fd787809a1
colerquinn <cole.r.quinn@outlook.com>
2026-03-18 11:28:51 -0700

    MSSA Project 2 Complete 18MAR

--- MSSAProject/18MARProject13.ps1
+++ MSSAProject/18MARProject13.ps1
@@ -4,6 +4,15 @@ foreach ($Share in $Shares) {
     Write-Host "Name: $($Share.name)" -ForegroundColor Green
     Write-Host "Path: $($Share.path)" -ForegroundColor Cyan
     Write-Host "Description: $($Share.Description)" -ForegroundColor Magenta
-    Write-Host "Scope Name: $($Share.scopename)" -ForegroundColor DarkYellow
+    Write-Host "Permissions:" -ForegroundColor DarkYellow
+    $Permissions = Get-SmbShareAccess -Name $Share.name
+    foreach ($Permission in $Permissions) {
+        Write-Host "$($Permission.AccountName): $($Permission.AccessControlType) ($($Permission.AccessRights))" -ForegroundColor DarkYellow
+    }
     Write-Host "----------------------------------------------------" -ForegroundColor White
 }
+#This script checks to see what shared folders exist and where they exist, as well
+#as the description of what kind of file it is. This also allows Administrators to
+#check to see who has permissions within the shared folders, see what their accesses
+#are, and what specific access rights they have. In this particular environment, there
+#are no access rights applied (indicated by the empty parentheses).
